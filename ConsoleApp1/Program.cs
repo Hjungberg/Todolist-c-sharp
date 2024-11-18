3fe7fc78914b8ee8eefb13c55c9c4b3939f9b335
Henrik Jungberg <henkesnya@gmail.com>
2024-11-18 17:45:35 +0100

    TodoList

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,42 @@
-﻿Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿using System;
+
+List<string> toDo = new List<string>();
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("--Todo-List--");
+    Console.WriteLine("-------------");
+    foreach (string item in toDo) {
+        Console.WriteLine(item);
+    }
+    Console.Write("Enter todo item (Write DONE to quit): ");
+    
+    var todoItem = Console.ReadLine();
+
+    if (todoItem == "DONE")
+    {
+        break;
+    }
+
+    if (todoItem == "SORT")
+    {
+        toDo.Sort();
+    }
+    else
+    {
+        toDo.Add(todoItem);
+    }
+}
+Console.Clear();
+Console.WriteLine("--Todo-List--");
+Console.WriteLine("-------------");
+int i = 0;
+foreach (string item in toDo)
+{
+    i++;
+    Console.Write(i + ": ");
+    Console.WriteLine(item);
+}
+Console.ReadKey();
+
