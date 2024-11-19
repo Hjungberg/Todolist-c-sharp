ac50a53ba1a5ff951d015c01dd1029461df44511
Henrik Jungberg <henkesnya@gmail.com>
2024-11-19 14:34:10 +0100

    Added remove item

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,8 @@ while (doLoop)
     Console.WriteLine("");
     Console.Write("Enter todo item (Write help for commands): ");
     
-    var todoItem = Console.ReadLine();
+    var todoItem = Console.ReadLine().Trim();
+    string removeItem = "";
 
     switch (todoItem)
     {
@@ -22,13 +23,36 @@ while (doLoop)
         case "help":
             Console.Clear();
             Console.WriteLine("       Commands");
-            Console.WriteLine("-------------------------");
-            Console.WriteLine(" Help - Show this text.");
-            Console.WriteLine(" Sort - Sort the list.");
-            Console.WriteLine(" Done - Finish the list.");
-            Console.WriteLine(" ------------------------");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine(" Help   - Show this text.");
+            Console.WriteLine(" Sort   - Sort the list.");
+            Console.WriteLine(" Delete - Remove item from list.");
+            Console.WriteLine(" Done   - Finish the list.");
+            Console.WriteLine(" -------------------------------");
             Console.WriteLine("Press any key to continue.");
             Console.ReadKey();
+            break;
+        case "delete":
+            Console.Write("Enter number of item to remove: ");
+            removeItem = Console.ReadLine();
+            bool isANumber = int.TryParse(removeItem, out int value);
+            if (isANumber)
+            {
+                if ((value - 1) < toDo.Count() && value > 0)
+                {
+                    toDo.RemoveAt(value - 1);
+                }
+                else
+                {
+                    Console.WriteLine("Number not in list.");
+                    Console.ReadKey();
+                }
+            } 
+            else 
+            {
+                Console.WriteLine($"{removeItem} is not a valid number");
+                Console.ReadKey();
+            }
             break;
         default:
             toDo.Add(todoItem);
