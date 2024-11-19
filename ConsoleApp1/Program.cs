7f68fbd9b26c83e47b61aba131fd3480b917f46a
Henrik Jungberg <henkesnya@gmail.com>
2024-11-19 17:28:49 +0100

    Cleaned up the code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,10 +9,9 @@ while (doLoop)
     Console.WriteLine("");
     Console.Write("Enter todo item (Write help for commands): ");
     
-    var todoItem = Console.ReadLine().Trim();
-    string removeItem = "";
+    string todoItem = Console.ReadLine().Trim();
 
-    switch (todoItem)
+    switch (todoItem.ToLower())
     {
         case "done":
             doLoop = false;
@@ -21,38 +20,10 @@ while (doLoop)
             toDo.Sort();
             break;
         case "help":
-            Console.Clear();
-            Console.WriteLine("       Commands");
-            Console.WriteLine("--------------------------------");
-            Console.WriteLine(" Help   - Show this text.");
-            Console.WriteLine(" Sort   - Sort the list.");
-            Console.WriteLine(" Delete - Remove item from list.");
-            Console.WriteLine(" Done   - Finish the list.");
-            Console.WriteLine(" -------------------------------");
-            Console.WriteLine("Press any key to continue.");
-            Console.ReadKey();
+            ShowHelp();
             break;
         case "delete":
-            Console.Write("Enter number of item to remove: ");
-            removeItem = Console.ReadLine();
-            bool isANumber = int.TryParse(removeItem, out int value);
-            if (isANumber)
-            {
-                if ((value - 1) < toDo.Count() && value > 0)
-                {
-                    toDo.RemoveAt(value - 1);
-                }
-                else
-                {
-                    Console.WriteLine("Number not in list.");
-                    Console.ReadKey();
-                }
-            } 
-            else 
-            {
-                Console.WriteLine($"{removeItem} is not a valid number");
-                Console.ReadKey();
-            }
+            DeleteItem(toDo);
             break;
         default:
             toDo.Add(todoItem);
@@ -68,12 +39,50 @@ static void ShowList(List<string> toDo)
 {
     int i = 0;
     Console.Clear();
-    Console.WriteLine("--Todo-List--");
-    Console.WriteLine("-------------");
+    Console.WriteLine("-------Todo List--------");
+    Console.WriteLine("------------------------");
     i = 0;
     foreach (string item in toDo)
     {
         i++;
         Console.WriteLine($" {i}: {item}");
     }
+}
+
+static void DeleteItem(List<string> toDo)
+{
+    Console.Write("Enter number of item to remove: ");
+    string removeItem = Console.ReadLine();
+    bool isANumber = int.TryParse(removeItem, out int value);
+    if (isANumber)
+    {
+        if ((value - 1) < toDo.Count() && value > 0)
+        {
+            toDo.RemoveAt(value - 1);
+        }
+        else
+        {
+            Console.WriteLine("Number not in list.");
+            Console.ReadKey();
+        }
+    }
+    else
+    {
+        Console.WriteLine($"{removeItem} is not a valid number");
+        Console.ReadKey();
+    }
+}
+
+static void ShowHelp()
+{
+    Console.Clear();
+    Console.WriteLine("       Commands");
+    Console.WriteLine("--------------------------------");
+    Console.WriteLine(" Help   - Show this text.");
+    Console.WriteLine(" Sort   - Sort the list.");
+    Console.WriteLine(" Delete - Remove item from list.");
+    Console.WriteLine(" Done   - Finish the list.");
+    Console.WriteLine(" -------------------------------");
+    Console.Write(" Press any key to continue.");
+    Console.ReadKey();
 }
